f1aac5432e124f3f3a9489749c598c3d83823a5a
Suriyan Laohaprapanon <suriyant@gmail.com>
2020-05-06 07:43:41 +0000

    bump to 0.2.0

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.16',
+    version='0.2.0',
 
     description='Classifying the Content of Domains',
     long_description=long_description,
@@ -96,6 +96,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities'
